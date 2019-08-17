28a3e32e2462cef410fc93514c5df3eb79d18f63
Liv Marx <oehrenmarks@gmail.com>
2019-08-17 16:47:57 -0400

    add css

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+
 <style lang="scss">
+body {
+  background-color: rgb(94, 217, 224);
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: rgb(170, 238, 238);
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
 </style>
