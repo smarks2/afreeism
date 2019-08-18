35b89f2177db9edf5e48c2abeb19623d53e14e39
Marks <smarks@bu.edu>
2019-08-18 12:39:19 -0400

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <h1>The Afreeist</h1>
-    <h3>Coming incredibly Soon or not</h3>
+    <h1>The Afreeist Society</h1>
+    <p>The Afreeist Society is a collection of individuals interested in the ideas and concepts 
+    of Afreeism.  Afreeism posits that the world is best described as causally
+     or stochastically deterministic and that this implies the absence of free will.</p>
   </div>
 </template>
 
