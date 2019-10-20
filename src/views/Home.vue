db2fca07fb17d9688cabf2f36368b8a086f29f99
Liv Marx <oehrenmarks@gmail.com>
2019-10-20 15:33:07 -0400

    Added text to Handbook link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,56 @@
 <template>
   <div class="home">
     <h1>The Afreeist Society</h1>
-    <p>The Afreeist Society is a collection of individuals interested in the ideas and concepts 
-    of Afreeism.  </p>
-    <p><b>What is afreeism?</b></p>
-    <p>Afreeism (pronounced ay'-free-ism) posits that the world is best described as causally
-     or stochastically deterministic and that this implies the absence of free will.  
-     </p>
-      <p><b>What are the implications of afreeism?</b></p>
     <p>
-     Without free will there can be no moral responsibility for one's past actions.  That is,
-     in Afreeism, there is no concept of desert.  No one deserves either punishment or rewards.
-     If we do punish or reward, it has to be justified by something other than the notion
-     that a person deserves punishment or reward.  This idea that no one deserves anything
+      The Afreeist Society is a collection of individuals interested in the ideas and concepts
+      of Afreeism.
+    </p>
+    <p>
+      <b>What is afreeism?</b>
+    </p>
+    <p>
+      Afreeism (pronounced ay'-free-ism) posits that the world is best described as causally
+      or stochastically deterministic and that this implies the absence of free will.
+    </p>
+    <p>
+      <b>What are the implications of afreeism?</b>
+    </p>
+    <p>
+      Without free will there can be no moral responsibility for one's past actions. That is,
+      in Afreeism, there is no concept of desert. No one deserves either punishment or rewards.
+      If we do punish or reward, it has to be justified by something other than the notion
+      that a person deserves punishment or reward. This idea that no one deserves anything
       affects how we envision social
-     policy and our own personal philosophy.  For example, afreeism implies:
-     </p>
-     <p>
-     1.  Our notion of crime and punishment should not be based on retribution, but rather on 
-     deterrence, rehabilitation, and (occasionally) on separation.
-     </p><p>
-     2.  Our notion of a fair distribution of society's benefits should not be based on what people
-     deserve, but rather on a bedrock of equality modified to the extent necessary to provide
-     incentives.
-     </p><p>
-     3.  Our relationships with other people should be based on tolerance.
-     </p><p>
-     4.  Our own lives should be animated by engagement, but also by self-acceptance.  Guilt,
-     regret, remorse, shame, do not make sense in a deterministic world.
-     </p><p>
-     </p><p>
-     </p>
-      <p><b>How can I learn more about afreesim?</b></p>
-    <p>If you 
-     wish to explore these ideas, a good starting point is the Afreeist Handbook.</p>
+      policy and our own personal philosophy. For example, afreeism implies:
+    </p>
+    <p>
+      1. Our notion of crime and punishment should not be based on retribution, but rather on
+      deterrence, rehabilitation, and (occasionally) on separation.
+    </p>
+    <p>
+      2. Our notion of a fair distribution of society's benefits should not be based on what people
+      deserve, but rather on a bedrock of equality modified to the extent necessary to provide
+      incentives.
+    </p>
+    <p>3. Our relationships with other people should be based on tolerance.</p>
+    <p>
+      4. Our own lives should be animated by engagement, but also by self-acceptance. Guilt,
+      regret, remorse, shame, do not make sense in a deterministic world.
+    </p>
+    <p></p>
+    <p></p>
+    <p>
+      <b>How can I learn more about afreesim?</b>
+    </p>
+    <p>
+      If you
+      wish to explore these ideas, a good starting point is the Afreeist Handbook.
+    </p>
     <routerLink :to="{name: 'Faq'}">
-    <button>Faq</button>
+      <button>Faq</button>
     </routerLink>
-    <routerLink :to="{name: 'Handbook'}">
-    </routerLink>
+    <br />
+    <routerLink :to="{name: 'Handbook'}">Handbook</routerLink>
   </div>
 </template>
 
@@ -49,5 +61,8 @@ export default {
 };
 </script>
 <style scoped>
-p {text-align:left; color:#000000;}
+p {
+  text-align: left;
+  color: #000000;
+}
 </style>
