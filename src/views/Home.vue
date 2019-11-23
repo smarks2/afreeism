7c13d4de733a4628a0e7db94f9b899785af9b563
Marks <smarks@bu.edu>
2019-11-23 09:47:59 -0500

    blah

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,6 +17,8 @@
       </tr>
     </table>
     <p>
+    <b>What is afreeism?</b>
+    <p>
       Afreeism (ay'-free-izm) is a simple idea.  Yet it is an idea so powerful that, if fully embraced, 
       it radically alters the 
       way we see the world. It changes how we view each other and how we treat one another. 
@@ -27,39 +29,61 @@
       rooted in modern scientific discovery. It is not an easy idea. It requires abandoning 
       a worldview that we probably have never questioned, a worldview that we take for granted, 
       a worldview that affects our actions on a daily basis.
-  </p>     
-  <p>    
+    </p>     
+    <p>    
       What is this radical yet ancient idea? It is simply this: we have no free will. 
       The universe is a web of causation dating back to the beginning of time. Our 
       actions are a part of this web. Although we feel that we are making decisions 
       on the basis of free will, this is just an illusion. We could not have acted 
       differently than we did.
-  </p>     
-  <p>
-      The implications of afreeism are profound.  Jettisoning the notion of free will means that:
-  </p>     
-  <p>
+    </p>   
+    <p>
+    <b>What does afreeism say about how to live life?</b>
+    </p>
+    <p>
+      When we give up the illusion of free will, our perspective on life changes.  We see that 
+      the notion that people deserve things (punishment, rewards, blame, retribution) no 
+      long makes sense.  And that applies to ourselves as well.  As a result:
+    </p>     
+    <p>
       •	We will support social policies that are both more humane and more effective. 
       This applies particularly in the area of criminal justice, but also to many other 
       areas of government policy. 
-   </p>     
-  <p>
+    </p>     
+    <p>
       •	We will act more generously 
       towards other people in our personal lives. In particular, we will become more tolerant and more 
       accepting of other people, their life situations, and their actions. 
-  </p>     
-  <p> 
+    </p>     
+    <p> 
       •	We will improve our attitude towards ourselves 
       by mitigating feelings of guilt, regret, remorse, and self-blame, all of which are impediments 
       to a joyful life. 
-   </p>     
-  <p>
+    </p>     
+    <p> 
+  These are just a few of the implications of afreeism.  Afreeism has much to say about our
+  place in the universe, our relationship to our fellow beings, and how we want to organize
+  society.
+    </p> 
+    <p>
+    <b>Who are we?</b>
+    </p>    
+    <p>
       The Afreeist Society is a collection of individuals interested in the ideas and ideals
-      of afreeism.
-  </p>   
+      of afreeism.  The goals of the Afreeist Society are:
+    </p>   
+    <p>
+    a. To explain afreeism.
+    </p>
+    <p>
+    b. To stimulate thinking and research about afreeism.
+    </p>   
+    <p>c. To promote the values and ideals of afreeism.
+    </p> 
     <p>
       <b>How can I learn more about afreesim?</b>
     </p>
+
     <p>
       If you
       wish to explore these ideas, a good starting point is the 
