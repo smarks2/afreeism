73cd7a66694b09b6bb3fb81b570dff31d0cce9b8
Marks <smarks@bu.edu>
2019-12-30 14:32:27 -0500

    blah

--- src/views/Topics/Basics/Autonomic_Robots.vue
+++ src/views/Topics/Basics/Autonomic_Robots.vue
@@ -2,24 +2,40 @@
   <div  id="Autonomic_Robots" class="TopicExplanation" >
     <div class="TopicHeading">Autonomy in a Robot World</div>
 
-<p>
-One way to examine how autonomy works in a deterministic universe is to 
-imagine how it would work in a world populated by robots.
-Of course, to a determinist, our world is a robot world.  Perhaps the only thing 
-that distinguishes humans from current robots is their complexity.  Robots can do 
-many of the same things that humans do.  Some things they can do better.  
-Others, not as well.  It is conceivable that one day we will develop robots 
-of such complexity that they will achieve consciousness and be able to express and feel emotion.  
-</p>
-<p>
-Even now we can create robots and imbue them with decision-making ability.  That is, we can create robots with algorithms for deciding.  We can ask such a robot to make a decision.  Using its algorithm, the robot will weigh many factors, will calculate outcomes, and will make a decision based on the projected outcome that maximizes whatever goal we specify.  In other words, the robot will be operating in much the same way that we do.  Its decision-making process may not be as complex as ours, but it is still making decisions.
-</p>
-<p>
-Looking at the robot through the lens of causation, we can see that the robot, when it comes to a decision node, observes various possible paths.  It weighs the costs and benefits of traveling one path versus another, and then it takes action, choosing one alternative over a number of others.  This is what we do too.
-</p>
-<p>
-However, when we look at what the robot did through that same lens of causation, we see that, given the algorithm that we imbued the robot with, given the alternatives that the robot faced, and given the predicted outcomes of each of those alternatives, the robot could have done only one thing.  We can look at the algorithm and the inputs and see that there was only one path that the robot could have taken.  Looking backwards in time, there was only one possible road.
-</p>
+      <p>
+      One way to examine how autonomy works in a deterministic universe is to 
+      imagine how it would work in a world populated by robots.
+      Of course, to a determinist, our world is a robot world.  Perhaps the only thing 
+      that distinguishes humans from current robots is their complexity.  Robots can do 
+      many of the same things that humans do.  Some things they can do better.  
+      Others, not as well.  It is conceivable that one day we will develop robots 
+      of such complexity that they will achieve consciousness and be able to express and feel emotion.  
+      </p>
+      <p>
+      Even now we can create robots and imbue them with decision-making ability.  
+      That is, we can create robots with algorithms for deciding.  We can 
+      ask such a robot to make a decision.  Using its algorithm, the robot 
+      will weigh many factors, will calculate outcomes, and will make a 
+      decision based on the projected outcome that maximizes whatever goal 
+      we specify.  In other words, the robot will be operating in much the 
+      same way that we do.  Its decision-making process may not be as complex 
+      as ours, but it is still making decisions.
+      </p>
+      <p>
+      Looking at the robot through the lens of causation, we can see that 
+      the robot, when it comes to a decision node, observes various possible 
+      paths.  It weighs the costs and benefits of traveling one path versus another, and then it takes action, choosing one alternative over a number of others.  This is what we do too.
+      </p>
+      <p>
+      However, when we look at what the robot did through that 
+      same lens of causation, we see that, given the algorithm 
+      that we imbued the robot with, given the alternatives that 
+      the robot faced, and given the predicted outcomes of each of 
+      those alternatives, the robot could have done only one thing.  
+      We can look at the algorithm and the inputs and see that there was 
+      only one path that the robot could have taken.  Looking backwards 
+      in time, there was only one possible road.
+      </p>
 
         <p><a href="#Back">Back to top.</a></p>
     </div>
